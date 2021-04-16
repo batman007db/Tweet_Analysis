3136b116363c7c45e5bf445446f03565747ee0a9
batman007db <dharmikbhanushali@gmail.com>
2021-04-17 01:48:41 +0530

    Update App_Streamlit.py

--- App_Streamlit.py
+++ App_Streamlit.py
@@ -172,10 +172,10 @@ def main():
         st.write("Total Neutral Tweets are : {}".format(len(df[df["Sentiment"]=="Neutral"])))
         
         # See the Extracted Data : 
-        if st.button("See the Extracted Data"):
+        
             #st.markdown(html_temp, unsafe_allow_html=True)
-            st.success("Below is the Extracted Data :")
-            st.write(df.head(50))
+        st.success("Below is the Extracted Data :")
+        st.write(df.head(50))
         
         
         # get the countPlot
@@ -188,21 +188,21 @@ def main():
         # Piechart 
         if st.button("Get Pie Chart for Different Sentiments"):
             st.success("Generating A Pie Chart")
-            """a=len(df[df["Sentiment"]=="Positive"])
+            a=len(df[df["Sentiment"]=="Positive"])
             b=len(df[df["Sentiment"]=="Negative"])
             c=len(df[df["Sentiment"]=="Neutral"])
             d=np.array([a,b,c])
             explode = (0.1, 0.0, 0.1)
             st.write(plt.pie(d,shadow=True,explode=explode,labels=["Positive","Negative","Neutral"],autopct='%1.2f%%'))
-            st.pyplot()"""
+            st.pyplot()
             
             
         # get the countPlot Based on Verified and unverified Users
         if st.button("Get Count Plot Based on Verified and unverified Users"):
-            """st.success("Generating A Count Plot (Verified and unverified Users)")
+            st.success("Generating A Count Plot (Verified and unverified Users)")
             st.subheader(" Count Plot for Different Sentiments for Verified and unverified Users")
             st.write(sns.countplot(df["Sentiment"],hue=df.IsVerified))
-            st.pyplot()"""
+            st.pyplot()
         
         
         ## Points to add 1. Make Backgroud Clear for Wordcloud 2. Remove keywords from Wordcloud
@@ -211,36 +211,36 @@ def main():
         # Create a Worlcloud
         if st.button("Get WordCloud for all things said about {}".format(Topic)):
             st.success("Generating A WordCloud for all things said about {}".format(Topic))
-            """text = " ".join(review for review in df.clean_tweet)
+            text = " ".join(review for review in df.clean_tweet)
             stopwords = set(STOPWORDS)
             text_newALL = prepCloud(text,Topic)
             wordcloud = WordCloud(stopwords=stopwords,max_words=800,max_font_size=70).generate(text_newALL)
             st.write(plt.imshow(wordcloud, interpolation='bilinear'))
-            st.pyplot()"""
+            st.pyplot()
         
         
         #Wordcloud for Positive tweets only
         if st.button("Get WordCloud for all Positive Tweets about {}".format(Topic)):
-            """st.success("Generating A WordCloud for all Positive Tweets about {}".format(Topic))
+            st.success("Generating A WordCloud for all Positive Tweets about {}".format(Topic))
             text_positive = " ".join(review for review in df[df["Sentiment"]=="Positive"].clean_tweet)
             stopwords = set(STOPWORDS)
             text_new_positive = prepCloud(text_positive,Topic)
             #text_positive=" ".join([word for word in text_positive.split() if word not in stopwords])
             wordcloud = WordCloud(stopwords=stopwords,max_words=800,max_font_size=70).generate(text_new_positive)
             st.write(plt.imshow(wordcloud, interpolation='bilinear'))
-            st.pyplot()"""
+            st.pyplot()
         
         
         #Wordcloud for Negative tweets only       
         if st.button("Get WordCloud for all Negative Tweets about {}".format(Topic)):
             st.success("Generating A WordCloud for all Positive Tweets about {}".format(Topic))
-            """text_negative = " ".join(review for review in df[df["Sentiment"]=="Negative"].clean_tweet)
+            text_negative = " ".join(review for review in df[df["Sentiment"]=="Negative"].clean_tweet)
             stopwords = set(STOPWORDS)
             text_new_negative = prepCloud(text_negative,Topic)
             #text_negative=" ".join([word for word in text_negative.split() if word not in stopwords])
             wordcloud = WordCloud(stopwords=stopwords,max_words=800,max_font_size=70).generate(text_new_negative)
             st.write(plt.imshow(wordcloud, interpolation='bilinear'))
-            st.pyplot()"""
+            st.pyplot()
         
         
         
