d6e1f898847ad3da4bd5f05d53c70c82db59431f
batman007db <dharmikbhanushali@gmail.com>
2021-05-19 22:47:43 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 mkdir -p ~/.streamlit/
 echo "\
 [general]\n\
-email = \"darekarabhishek@gmail.com\"\n\
+email = \"dharmikbhanushali@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
 echo "\
 [server]\n\
